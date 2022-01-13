89129198043edbe4c56a0de1c58630e31d183b44
Thomas Moran <thomasmoran@Thomass-MacBook-Air.local>
2022-01-13 06:51:24 +0000

    removed import

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 // TODO: Add your import statements here.
 import { getRoles, getCompanies } from "./modules/salaryData.js";
-import { getAverageSalaryByRole } from ".modules/workAroundModule.js";
  
 // TODO: Get the companies and roles using the salaryData module.
 const companies = getRoles();
@@ -55,7 +54,7 @@ function updateResults(){
   if (!company || !role) { return; }
 
   // TODO: Use the workAroundModule functions to calculate the needed data.
-  const averageSalaryByRole = 10;
+  const averageSalaryByRole = 0;
   const averageSalaryByCompany = 0;
   const salary = 0;
   const industryAverageSalary = 0;
