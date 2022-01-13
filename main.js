85a0c235a9e7f8cd272464e7360daa8fe8c85a53
Thomas Moran <thomasmoran@Thomass-MacBook-Air.local>
2022-01-13 06:40:47 +0000

    added getAverageSalaryByRole

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // TODO: Add your import statements here.
 import { getRoles, getCompanies } from "./modules/salaryData.js";
+import {getAverageSalaryByRole} from ".modules/workAroundModule.js";
  
 // TODO: Get the companies and roles using the salaryData module.
 const companies = getRoles();
